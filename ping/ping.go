199192bf8d1ced69343204807fc6e2438ce8d56e
Yasin Kızılkaya <miya>
2018-03-31 12:36:45 +0300

    refactor code

--- ping/ping.go
+++ ping/ping.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func Handle(w http.ResponseWriter, r *http.Request) {
+func Handle(w http.ResponseWriter, _ *http.Request) {
 	info := fmt.Sprintf("{ Time: %s }", time.Now().Format(time.RFC3339))
 	w.Write([]byte(info))
 }
